e7c014886cdb9f289bbc44bf3edc17040076a4c0
Andreea Nicolescu <andreea.nicolescu@cyberint.ro>
2022-02-10 13:37:16 +0200

    removed duplicates

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-<<<<<<< HEAD
-=======
 import router from './router'
->>>>>>> c35d19e9dc66f659956a0f90d4cf586e8beecd04
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
-<<<<<<< HEAD
-=======
   router,
->>>>>>> c35d19e9dc66f659956a0f90d4cf586e8beecd04
   render: h => h(App)
 }).$mount('#app')
